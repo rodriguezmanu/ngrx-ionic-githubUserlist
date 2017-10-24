408bd853abda528d0c4dd9a335d1b58c5273be64
Manuel Rodriguez <rodriguez.manu@gmail.com>
2017-10-24 18:26:05 -0300

    Adding improvements for users state

--- src/app/reducers/users.ts
+++ src/app/reducers/users.ts
@@ -3,10 +3,32 @@ import { User } from '../models/user';
 
 export type Action = UsersActions.All;
 
-export function reducerUsers(state = [User], action: Action) {
+export interface State {
+  loading: boolean;
+  users: User[];
+}
+
+const initialState: State = {
+  loading: false,
+  users: []
+}
+
+export function reducerUsers(state = initialState, action: Action): State {
   switch(action.type) {
+    case UsersActions.LOAD: {
+      console.log(state, action)
+      return {
+        ...state,
+        loading: true,
+      }
+    }
     case UsersActions.LOAD_SUCCESS: {
-      return action.payload;
+      console.log(state, action)
+      return {
+        ...state,
+        loading: false,
+        users: action.payload
+      }
     }
 
     default: {
